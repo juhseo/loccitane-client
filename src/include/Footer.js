732b25b309c41ce3bf179e2298fc3e76e776914a
juh <juhsseo@gmail.com>
2022-01-18 17:21:36 +0900

    main반완료

---
+++ src/include/Footer.js
@@ -0,0 +1,111 @@
+function Footer(){
+    return(
+        <footer>
+            <div id="ft_top">
+                <div id="ft_left" className="cter">
+                    <p>INIPAY 에스크로 가입 확인</p>
+                    <p>
+                        록시땅 공식몰(kr.loccitane.com)은 귀하의 결제 정보 보안을<br/>
+                        최상의 상태로 유지하기 위하여,<br/>
+                        결제 시, (주)이니시스의 결제대금 예치서비스인<br/>
+                        에스크로 서비스를 제공하고 있습니다.<br/>
+                        -
+                    </p>
+                    <p>
+                        결제대금예치업 등록번호 : 02-006-00013<br/>
+                        통신판매번호 :  2008-서울강남-0844
+                    </p>
+                </div>
+                <div id="ft_mid" className="cter">
+                    <p>록시땅 공식몰 회원 5% 할인 혜택</p>
+                    <p>
+                        록시땅 코리아 공식몰에 가입하여 구매하시는 모든 분들께<br/> 
+                        5% 할인 혜택을 상시 제공하고 있습니다.<br/>
+                        공식몰에서만 진행되는 특별 프로모션도 놓치지 마세요.
+                    </p>
+                </div>
+                <div id="ft_right" className="cter">
+                    <p>고객 문의</p>
+                    <p>
+                        록시땅 대표전화 : (02) 2054-0500<br/>
+                        록시땅 대표메일 :loccitane@loccitane.co.kr<br/>
+                        록시땅 플러스친구: @록시땅<br/>
+                        *업무 시간: 평일 오전 10시~오후 5시, 공휴일 제외<br/>
+                        결제 문의 : 1588-4954<br/>
+                    </p>
+                </div>
+            </div>
+            <div id="ft_mid_01">
+                <div>
+                    <p>록시땅 소개</p>
+                    <ul>
+                        <li>록시땅 소개</li>
+                        <li>록시땅 팩토리 투어</li>
+                        <li>록시땅 멤버쉽 프로그램</li>
+                        <li>록시땅 리사이클 캠페인</li>
+                        <li>스파 록시땅</li>
+                        <li>록시땅 그룹</li>
+                        <li>투자자</li>
+                    </ul>
+                </div>
+                <div>
+                    <p>고객 지원</p>
+                    <ul>
+                        <li>이용약관</li>
+                        <li>개인정보처리방침</li>
+                        <li>온라인 문의</li>
+                        <li>자주하는 질문</li>
+                        <li>배송조회</li>
+                    </ul>
+                </div>
+                <div>
+                    <p>기업 구매 문의</p>
+                    <ul>
+                        <li>기업 선물 문의</li>
+                        <li>호텔 어메니티</li>
+                        <li>기업 선물 추천</li>
+                    </ul>
+                </div>
+                <div>
+                    {/* <!-- 이모티콘 넣기 --> */}
+                    <p>SNS</p>
+                    <ul>
+                        <li>facebook</li>
+                        <li>YouTube</li>
+                        <li>Instagram</li>
+                        <li>Kakaotalk</li>
+                        <li>Twitter</li>
+                    </ul>
+                </div>
+            </div>
+            <div id="ft_mid_02">
+                <ul>
+                    <li>사이트 맵</li>
+                    <li>이용약관</li>
+                    <li>개인정보처리방침</li>
+                    <li>International L'OCCITANE</li>
+                    <li>에스크로 가입 확인</li>
+                </ul>
+            </div>
+            <div id="ft_bottom">
+                <img src="img/small_logo.png" alt="small_logo"/>
+                <div>
+                    <p>상호: 록시땅코리아 유한책임회사 | 사업자 등록 번호 211-87-74234 | 대표자 : 안드레조셉호프만 | 주소: 서울특별시 강남구 테헤란로 87길 36(삼성동) 도심공항타워 21층(06164)</p>
+                    <p>| 대표전화 :(02)2054-0500 | 대표메일 : LOCCITANE@LOCCITANE.CO.KR | 결제관련문의 : 1588-4954 | 주문관련문의 : (02)2054-0500 | 통신판매번호 : 2008-서울강남-0844 |</p>
+                </div>
+                <div>
+                    {/* <!-- 아이콘 넣기 --> */}
+                    <button><i></i>좋아요</button>
+                    <p>
+                        632만명이 좋아합니다.<br/>
+                        친구들이 무엇을 좋아하는지 알아보려면 가입하기
+                    </p>
+                </div>
+                <div>
+                    L'OCCITANE EN PROVENCE © COPYRIGHT 2022
+                </div>
+            </div>
+    </footer>
+    );
+}
+export default Footer;
