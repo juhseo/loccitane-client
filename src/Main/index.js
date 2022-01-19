f0781d863fbd54242d2690517fca2e359baf8802
juh <juhsseo@gmail.com>
2022-01-20 01:35:38 +0900

    상단완성

--- src/Main/index.js
+++ src/Main/index.js
@@ -30,7 +30,7 @@ function MainPage(){
     
     return(
             <div id="main">
-                <Carousel autoplay={true} autoplaySpeed={3000}>
+                {/* <Carousel autoplay={true} autoplaySpeed={3000}>
                     {banners.map((banner,index)=>{
                         return(
                             <div id="visual" key={index}>
@@ -39,9 +39,56 @@ function MainPage(){
                         );
                     })}
                 
-                </Carousel>
-                <div id="product" className="innerCon">
-                    <h1>그린 조명 최신상품</h1>
+                </Carousel> */}
+                <div id="sec01">
+                    <img src="img/sec01_01.webp" />
+                    <div id="sec01_mini_bann">
+                        <p>
+                            2022 
+                            <br/>SHEA FOR GOOD
+                        </p>
+                        <p>
+                            록시땅 X 나난<br/>
+                            한국 단독 첫 국내 아티스트<br/>
+                            콜라보레이션 에디션
+                        </p>
+                        <span>자세히보기</span>
+                    </div>
+                </div>
+                
+                <div id="sec02">
+                    <div id="sec02_left">
+                        <img src="img/sec01_icon01.png"/>
+                        <div className="sec02_mini_text">
+                            <p>전구매 무료배송 & 설 연휴 공지</p>
+                            <p>
+                                록시땅 공식몰 무료배송 서비스.<br/>
+                                설 연휴 공식몰 배송과 매장 휴점 안내는 해당 배너를 클릭후<br/>확인 부탁드립니다.
+                            </p>
+                        </div>
+                    </div>
+                    <div id="sec02_right">
+                        <img src="img/sec01_icon02.png"/>
+                        <div className="sec02_mini_text">
+                            <p>록시땅 공식몰 단독 혜택</p>
+                            <p>
+                                첫구매 시, 미니 필로우 미스트 증정 / 공식몰 상시 5% 할인 <br/>
+                                전 구매 고객 베스트 셀러 샘플 3종 증정
+                            </p>
+                        </div>
+                    </div>
+                </div>
+
+                <div id="sec03" className="innerCon">
+                    <div id="sec03_submenu">
+                        <ul>
+                            <li>SHEA FOR GOOD GIFT</li>
+                            <li>NEW SHEA FOR GOOD</li>
+                            <li>NEW 릴랙싱 롤온</li>
+                        </ul>
+                    </div>
+                    <h1>SHEA FOR GOOD GIFT</h1>
+                    <div id="underbar"></div>
                     <div id="product-list">
                         {
                         products.map(product => {
--- src/include/Header.js
+++ src/include/Header.js
@@ -2,11 +2,23 @@ import { Link } from "react-router-dom";
 function Header(){
     return(
         <header>
+            <div id="header_top">공식 온라인몰 회원 가입 시, 상시 5%할인 혜택</div>
             <div className="innerCon">
                 <h1><Link to="/"><img src="img/logo.png"/></Link></h1>
                 <ul>
+                    <li>로그인/회원정보</li>
                     <li><Link to="/upload">상품등록하기</Link></li>
                     <li>상품보기</li>
+                </ul>
+            </div>
+            <div id="sub_menu">
+                <ul>
+                    <li>2022 NEW SHEA FOR GOOD</li>
+                    <li>베스트셀러</li>
+                    <li>이달의 프로모션</li>
+                    <li>선물 추천</li>
+                    <li>라인별 컬렉션</li>
+                    <li>록시땅 소개</li>
                 </ul>
             </div>
         </header>
